ed356ff131b1f20e2b4016e468bd5d31c008a5d5
geekbrains <dmitriy.lim@gmail.com>
2022-05-28 16:57:09 +0300

    first commit

--- src/main/java/ru/geekbrains/lim/task2/Executor.java
+++ src/main/java/ru/geekbrains/lim/task2/Executor.java
@@ -4,6 +4,8 @@ import ru.geekbrains.lim.task2.exceptions.ArrayLengthException;
 
 public class Executor {
     private static final int ARRAY_LENGTH = 4;
+    public static final String WRONG_ARRAY_SIZE_MESSAGE = "Размер массива не равен %d x %d";
+    public static final String ARRAY_VALUE_IS_NOT_NUMBER_MESSAGE = "Значение массива [%d][%d]: %s не является числом ";
 
     public static void main(String[] args) {
         String[][] s = {
@@ -22,14 +24,14 @@ public class Executor {
     private static int sumArray(String[][] arr) throws ArrayLengthException {
         int s = 0;
         if (arr.length != ARRAY_LENGTH || arr[0].length != ARRAY_LENGTH) {
-            throw new ArrayLengthException(String.format("Размер массива не равен %d x %d", ARRAY_LENGTH, ARRAY_LENGTH));
+            throw new ArrayLengthException(String.format(WRONG_ARRAY_SIZE_MESSAGE, ARRAY_LENGTH, ARRAY_LENGTH));
         }
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[0].length; j++) {
                 try {
                     s += Integer.parseInt(arr[i][j]);
                 } catch (NumberFormatException e) {
-                    System.out.println(String.format("Значение массима [%d][%d]: %s не является числом ", i, j, arr[i][j]));
+                    System.out.println(String.format(ARRAY_VALUE_IS_NOT_NUMBER_MESSAGE, i, j, arr[i][j]));
                     throw new RuntimeException();
                 }
             }
